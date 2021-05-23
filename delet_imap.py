8c6b4a668ac5b3d7262beb80dfe75ed2b9e157e5
parse-hardik <f20180062@hyderabad.bits-pilani.ac.in>
2021-05-23 23:18:28 +0530

    GetUnseenMessages and Attach image with MIMEImage

--- delet_imap.py
+++ delet_imap.py
@@ -16,6 +16,20 @@ def fetchMail(username, password):
     mailbox.close()
     mailbox.logout()
 
+def getUnseen(username, password):
+    mailbox = imaplib.IMAP4_SSL(pop3_server, pop3_port)
+    mailbox.login(username, password)
+    mailbox.select('Inbox')
+    result, data = mailbox.search(None, '(UNSEEN)')
+    if result == "OK":
+        for uid in data[0].split():
+            result, data = mailbox.fetch(uid, '(RFC822)')
+            msg = email.message_from_bytes(data[0][1])
+            print(msg)
+            print(30*'*')
+            result, data = mailbox.store(uid, '-FLAGS', '\\SEEN')
+
 username = input("Enter Username: ")
 password = getpass.getpass(prompt="Enter Password: ")
-fetchMail(username, password)
+# fetchMail(username, password)
+getUnseen(username, password)
--- smtp.py
+++ smtp.py
@@ -3,6 +3,8 @@ import smtplib
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+import base64
 
 smtp_server = 'smtp.gmail.com'
 smtp_port = 587
@@ -16,6 +18,19 @@ def sendMail(sender, receiver):
     part = MIMEText('text', 'plain')
     part.set_payload(message)
     msg.attach(part)
+    with open('/Users/Hardik/Desktop/kriti.jpg', 'rb') as f:
+        # set attachment mime and file name, the image type is png
+        mime = MIMEImage(f.read(), 'jpg')
+        # add required header data:
+        mime.add_header('Content-Disposition', 'attachment', filename='kriti.jpg')
+        mime.add_header('X-Attachment-Id', '0')
+        mime.add_header('Content-ID', '<0>')
+        # read attachment file content into the MIMEBase object
+        mime.set_payload(f.read())
+        # encode with base64
+        
+        # add MIMEBase object to MIMEMultipart object
+        msg.attach(mime)
     session = smtplib.SMTP(smtp_server, smtp_port)
     session.ehlo()
     session.starttls()
